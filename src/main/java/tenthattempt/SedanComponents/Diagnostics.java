5b8e44e658df9b75f24275ad74ece88f91a7c5f3
Jaylon A <35003843+ja-423@users.noreply.github.com>
2024-06-08 12:21:49 -0400

    Finally got some user input

--- src/main/java/tenthattempt/SedanComponents/Diagnostics.java
+++ src/main/java/tenthattempt/SedanComponents/Diagnostics.java
@@ -1,8 +1,40 @@
 package tenthattempt.SedanComponents;
 
 public class Diagnostics {
-    public boolean CheckEngine;
-    public boolean IsOilLow;
-    public boolean tirePressureLow;
-    public boolean IsFuseForHeadLightBlown;
+    boolean CheckEngine;
+    boolean oilLow;
+    boolean tirePressureLow;
+    boolean fuseForHeadLightBlown;
+
+    public boolean isCheckEngine() {
+        return CheckEngine;
+    }
+
+    public boolean isoilLow() {
+        return oilLow;
+    }
+
+    public boolean istirePressureLow() {
+        return tirePressureLow;
+    }
+
+    public boolean IsFuseForHeadLightBlown() {
+        return fuseForHeadLightBlown;
+    }
+
+    public void setCheckEngine(boolean CheckEngine) {
+        this.CheckEngine = CheckEngine;
+    }
+
+    public void setoilLow(boolean oilLow) {
+        this.oilLow = oilLow;
+    }
+
+    public void setTirePressureLow(boolean tirePressureLow) {
+        this.tirePressureLow = tirePressureLow;
+    }
+
+    public void setfuseForHeadLightBlown(boolean fuseForHeadLightBlown) {
+        this.fuseForHeadLightBlown = fuseForHeadLightBlown;
+    }
 }
