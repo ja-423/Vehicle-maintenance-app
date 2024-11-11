ddc1fa5d2576a91c9fb0ba6e065320aa9da3ea4b
Jaylon A <35003843+ja-423@users.noreply.github.com>
2024-11-11 14:26:13 -0500

    Dark mode (this took to long), dummy data for the table

--- ui/src/components/LogsView.vue
+++ ui/src/components/LogsView.vue
@@ -12,12 +12,12 @@
     data() {
       return {
         // Note `isActive` is left out and will not appear in the rendered table
-        fields: ['first_name', 'last_name', 'age'],
+        fields: ['Title', 'Description', 'Date'],
         items: [
-          { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
-          { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-          { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
-          { isActive: true, age: 38, first_name: 'Jami', last_name: 'Carney' }
+          { isActive: true, Date: 40, Title: 'Working Hard', Description: 'This does something' },
+          { isActive: false, Date: 21, Title: 'Hardly Working', Description: 'THis does not' },
+          { isActive: false, Date: 89, Title: 'Biding Time', Description: 'This may work' },
+          { isActive: true, Date: 38, Title: 'New Wrist', Description: 'This might now' }
         ]
       }
     }
